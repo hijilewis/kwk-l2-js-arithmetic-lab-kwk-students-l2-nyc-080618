6a0c6432c893464668585bb52485058d585c53da
Pooja  Tetali <pooja.tetali@gmail.com>
2018-08-08 14:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,3 @@ You should NOT try to redefine any of the above variables.
 var newID = oldID +1000000000; 
 var ageIsValid = currentAge.isInteger(); 
 
-if (ageIsValid == true){
-  
-}
-//var randomNumber = Math.random()
