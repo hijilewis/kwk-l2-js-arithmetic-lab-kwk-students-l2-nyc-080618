5b0ee243c3141a8ae11b49ca935cc87197d5c9c6
Pooja  Tetali <pooja.tetali@gmail.com>
2018-08-08 14:43:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ var newID = oldID +1000000000;
 var ageIsValid = Number.isInteger(currentAge); 
 var randomNumber = Math.floor(Math.random() * 20) + 1;
 var randomInteger = Math.floor(randomNumber); 
-
+var randomUserID = randomInteger + 1000000000; 
